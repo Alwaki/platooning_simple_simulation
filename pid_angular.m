90d00dd2d9608d73e04912971c4afc333b99b039
Alexander <59147412+Alwaki@users.noreply.github.com>
2021-09-12 20:36:41 +0200

    Update pid_angular.m

--- pid_angular.m
+++ pid_angular.m
@@ -1,20 +1,30 @@
-function [omega] = pid_angular(dtheta)
+function [output_control, error] = pid_angular(dtheta, prev_error, prev_control)
 %   Angular PID: Calculates the angular control
 %   This function will calculate the angular velocity control for 
 %   a given difference in angles.
 
-% Specify proportional constant
+% Saturation limits (model-specific)
+limit = 1.82;
+
+% Specify constants
 Kp = 2;
+Ki = 0.2;
+T = 0.1;
+
+% Rename input parameter
+error = dtheta;
 
 % Calculate control output
-omega = Kp * dtheta;
+output_control = Kp * error; %+ prev_control + Ki * (error + prev_error)* T / 2;
 
 % Limit output
-if(omega > 2)
-    omega = 2;
-elseif(omega < -2)
-    omega = -2;
+if(output_control > limit)
+    output_control = limit;
+elseif(output_control < -limit)
+    output_control = -limit;
 end
 
 end
+
+
 
